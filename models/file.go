372b658822d834b2e712ce5b1337eedec0b2a2c2
romirkul <ommirkulr@gmail.com>
2024-11-18 20:10:06 +0500

    initialized repos

--- models/file.go
+++ models/file.go
@@ -1,7 +1,7 @@
 package models
 
 type File struct {
-	FilePath string
-	Size     float32
-	MimeType string
+	FilePath string  `json:"file_path"`
+	Size     float32 `json:"size"`
+	MimeType string  `json:"mimetype"`
 }
--- models/response.go
+++ models/response.go
@@ -1,9 +1,9 @@
 package models
 
 type Response struct {
-	FileName    string
-	ArchiveSize float32
-	TotalSize   float32
-	TotalFiles  float32
-	Files       []File
+	FileName    string  `json:"filename"`
+	ArchiveSize float32 `json:"archive_size"`
+	TotalSize   float32 `json:"total_size"`
+	TotalFiles  float32 `json:"total_files"`
+	Files       []File  `json:"files"`
 }
